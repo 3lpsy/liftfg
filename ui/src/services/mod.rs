4cedf806130767cba825556dc34ad250e141459c
3lpsy <8757851+3lpsy@users.noreply.github.com>
2025-02-24 00:25:38 -0600

    generic service method reuse `call`

--- ui/src/services/mod.rs
+++ ui/src/services/mod.rs
@@ -11,6 +11,7 @@ use fgutils::{
 };
 use serde_wasm_bindgen::{from_value, to_value};
 use validator::ValidationErrors;
+use wasm_bindgen::JsValue;
 
 // Generic Get
 pub async fn get<T, R>(command: &str, args: Option<T>) -> Result<R, ValidationErrors>
@@ -20,6 +21,24 @@ where
 {
     let params = args.unwrap_or_default().to_params();
     let args = to_value(&params).expect("Failed to convert RequestParams to JsValue");
+    call::<R>(command, args).await
+}
+
+// Generic Post
+pub async fn post<T, R>(command: &str, args: T) -> Result<R, ValidationErrors>
+where
+    T: RequestableData,
+    R: ResponsableData,
+{
+    let req = args.as_request();
+    let args = to_value(&req).expect("Failed to convert RequestParams to JsValue");
+    call::<R>(command, args).await
+}
+
+pub async fn call<R>(command: &str, args: JsValue) -> Result<R, ValidationErrors>
+where
+    R: ResponsableData,
+{
     match invoke(command, args).await {
         Ok(result) => match from_value::<ResponseData<R>>(result.clone()) {
             Ok(response) => match (response.data, response.errors) {
@@ -43,51 +62,6 @@ where
         },
         Err(e) => {
             warn(&format!("Error: {:?}", &e));
-            let re =
-                from_value::<ResponseData<ValidationErrors>>(e.clone()).unwrap_or(ResponseData {
-                    data: None,
-                    errors: Some(verrors(
-                        VALIDATION_REQUEST_FIELD,
-                        VALIDATION_PARSING_CODE,
-                        format!("Error parsing request error: {:?}", e),
-                    )),
-                });
-            // theoretically, only errors should ever be populated
-            Err(re.errors.unwrap())
-        }
-    }
-}
-
-// Generic Post
-pub async fn post<T, R>(command: &str, args: T) -> Result<R, ValidationErrors>
-where
-    T: RequestableData,
-    R: ResponsableData,
-{
-    let req = args.as_request();
-    let args = to_value(&req).expect("Failed to convert RequestParams to JsValue");
-    match invoke(command, args).await {
-        Ok(result) => match from_value::<ResponseData<R>>(result.clone()) {
-            Ok(response) => match (response.data, response.errors) {
-                (Some(data), _) => Ok(data),
-                (_, Some(errors)) => Err(errors),
-                (None, None) => Err(verrors(
-                    VALIDATION_REQUEST_FIELD,
-                    VALIDATION_PARSING_CODE,
-                    "Error parsing result: No data or errors".to_string(),
-                )),
-            },
-            Err(e) => {
-                info(&format!("{:?}", &result));
-                let e = verrors(
-                    VALIDATION_REQUEST_FIELD,
-                    VALIDATION_PARSING_CODE,
-                    format!("Error parsing result: {:?}", e),
-                );
-                Err(e)
-            }
-        },
-        Err(e) => {
             let re =
                 from_value::<ResponseData<ValidationErrors>>(e.clone()).unwrap_or(ResponseData {
                     data: None,
