2ea80932bd70488f5371e7c7b236644acbd3ee66
3lpsy <8757851+3lpsy@users.noreply.github.com>
2025-03-02 17:41:22 -0600

    Centered onboarding views w/ correct height for no dock

--- ui/src/views/onboard.rs
+++ ui/src/views/onboard.rs
@@ -5,7 +5,7 @@ use dioxus::prelude::*;
 #[component]
 pub fn OnboardIndexView() -> Element {
     rsx! {
-        div { class: "hero",
+        div { class: "hero h-full align-center",
             div { class: "hero-content text-center",
                 div { class: "max-w-sm",
                     h1 { class: "text-5xl font-bold", "LiftFG" },
@@ -32,7 +32,7 @@ pub fn OnboardIndexView() -> Element {
 #[component]
 pub fn OnboardProfileCreateView() -> Element {
     rsx! {
-        div { class: "hero",
+        div { class: "hero h-full align-center",
             div { class: "hero-content flex-col",
                 div { class: "text-center",
                     h1 { class: "text-5xl font-bold", "Create Profile" }
